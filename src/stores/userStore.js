f0fa799dd78e82bed3e0082e439305835e86932a
gyutae <qoxowkd0716@gmail.com>
2024-01-26 03:31:45 +0900

    Feat: 유저 목록 가져오기 성공

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,43 +1,20 @@
 import { writable } from 'svelte/store';
-import { jwtDecode } from 'jwt-decode';
-
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return '';
-}
-
-function isAdminUser() {
-  const token = getCookie('Authorization');
-  if (!token) return false;
-
-  try {
-    const decoded = jwtDecode(token);
-    return decoded.role === 'ADMIN';
-  } catch (error) {
-    console.error('JWT 디코딩 중 오류 발생: ', error);
-    return false;
-  }
-}
+import { getCookie } from 'svelte-cookie';
+const HOST = 'http://localhost:8080';
 
 function createUserStore() {
   const { subscribe, set } = writable([]);
 
   async function fetchUsers() {
-    if (!isAdminUser()) {
-      console.error('권한 없음: ADMIN 역할이 필요합니다.');
-      return;
-    }
-
     try {
-      const response = await fetch('http://localhost:8080/api/v1/admin/users', {
+      const response = await fetch(HOST + '/api/v1/admin/users', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${getCookie('Authorization')}`,
+          Authorization: getCookie('Authorization'),
         },
       });
+
       if (!response.ok) {
         console.error(
           '유저 목록을 가져오는 데 실패했습니다: ',
@@ -46,6 +23,7 @@ function createUserStore() {
         );
         return;
       }
+
       const data = await response.json();
       set(data);
     } catch (error) {
