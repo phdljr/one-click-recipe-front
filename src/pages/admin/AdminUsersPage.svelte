30d29a9dee163d57c69cd9166169fc594e8d7fab
gyutae <qoxowkd0716@gmail.com>
2024-01-26 15:33:34 +0900

    Feat: UI 변경

--- src/pages/admin/AdminUsersPage.svelte
+++ src/pages/admin/AdminUsersPage.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Button from '@smui/button';
   import { Link } from 'svelte-routing';
   import { onMount } from 'svelte';
   import { userStore } from '../../stores/userStore';
@@ -22,84 +23,85 @@
   {#if $userStore.length === 0}
     <p>로딩 중이거나 유저가 없습니다.</p>
   {:else}
-    <ul class="user-list">
+    <div class="user-table">
       {#each $userStore as user (user.id)}
-        <li class="user-item">
-          <div class="user-id">{user.id}</div>
-          <div class="user-email">{user.email}</div>
-          <div class="user-nickname">{user.nickname}</div>
+        <div class="user-row">
+          <div class="user-info">
+            <span>{user.id}</span>
+            <span>{user.email}</span>
+            <span>{user.nickname}</span>
+          </div>
           <div class="user-role">
             <select bind:value={selectedRole[user.id]}>
               {#each roleOptions as role}
-                <option value={role} selected={role === user.role}
-                  >{role}</option
-                >
+                <option value={role}>{role}</option>
               {/each}
             </select>
           </div>
-          <div class="user-action">
-            <button on:click={() => changeUserRole(user.id)}>역할 변경</button>
+          <div class="user-actions">
+            <Button variant="raised" on:click={() => changeUserRole(user.id)}
+              >역할 변경</Button
+            >
             <Link to={`/admin/orders/${user.id}`}>주문 목록</Link>
           </div>
-        </li>
+        </div>
       {/each}
-    </ul>
+    </div>
   {/if}
 </main>
 
 <style>
   main {
-    max-width: 800px;
+    width: 1200px;
     margin: 0 auto;
     padding: 20px;
     text-align: center;
   }
 
-  .user-list {
-    width: 1000px;
-    list-style: none;
-    padding: 0;
+  .user-table {
+    display: flex;
+    flex-direction: column;
   }
 
-  .user-item {
-    background-color: #f4f4f4;
-    border: 1px solid #ddd;
-    padding: 10px;
-    margin-bottom: 10px;
-    border-radius: 4px;
+  .user-row {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom: 1px solid #ddd;
+    padding: 10px;
   }
 
-  .user-id,
-  .user-email,
-  .user-nickname,
-  .user-role,
-  .user-action {
-    flex-basis: 20%;
-    text-align: left;
+  .user-info {
+    display: flex;
+    flex: 1;
+    justify-content: space-between;
   }
 
-  .user-id,
-  .user-nickname {
-    overflow: hidden;
+  .user-info span {
+    flex-basis: 30%;
     text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
-  .user-email {
-    font-weight: bold;
     overflow: hidden;
-    text-overflow: ellipsis;
     white-space: nowrap;
   }
 
   .user-role {
+    flex-basis: 8%;
     background-color: #e0e0e0;
     padding: 5px 10px;
     border-radius: 20px;
     font-size: 0.85em;
+  }
+
+  .user-role select {
+    padding: 5px;
+    border-radius: 4px;
+    border: 1px solid #ddd;
+  }
+
+  .user-actions {
+    flex-basis: 20;
+    display: flex;
+    gap: 10px;
   }
 
   .user-role select {
@@ -108,10 +110,10 @@
     border: 1px solid #ddd;
   }
 
-  .user-action button {
-    padding: 5px 10px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
+  @media (max-width: 600px) {
+    .user-row {
+      flex-direction: column;
+      align-items: flex-start;
+    }
   }
 </style>
