24c5227f38b5ecae184f6a4a57f7b4ccf869c620
gyutae <qoxowkd0716@gmail.com>
2024-01-27 18:32:02 +0900

    Feat: 좋아요 이미지 생성

--- src/components/recipe/RecipeCard.svelte
+++ src/components/recipe/RecipeCard.svelte
@@ -6,10 +6,15 @@
     Media,
     PrimaryAction,
   } from '@smui/card';
-  import IconButton, { Icon } from '@smui/icon-button';
   import { navigate } from 'svelte-routing';
 
   export let recipe;
+
+  let liked = false;
+
+  function toggleLike() {
+    liked = !liked;
+  }
 </script>
 
 <div class="container">
@@ -38,14 +43,23 @@
         {recipe.writer}
       </span>
       <ActionIcons>
-        <IconButton
-          toggle
+        <button
+          on:click={toggleLike}
           aria-label="Add to favorites"
           title="Add to favorites"
         >
-          <Icon class="material-icons" on>favorite</Icon>
-          <Icon class="material-icons">favorite_border</Icon>
-        </IconButton>
+          {#if liked}
+            <img
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/3d9f7600f50823d72baaf50d03574a80c82377d898f89bc9faa5a053366b95c0?"
+              class="like-img"
+            />
+          {:else}
+            <img
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/9ff117ff108af49770015cb69ac2d18bf764d4ea8da4cbfd81705a20ed6664f1?"
+              class="like-img"
+            />
+          {/if}
+        </button>
       </ActionIcons>
     </Actions>
   </Card>
@@ -55,5 +69,17 @@
   .writer {
     color: rgb(0, 0, 0);
     font-weight: bold;
+  }
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+  .like-img {
+    aspect-ratio: 1.11;
+    object-fit: contain;
+    object-position: center;
+    width: 20px;
   }
 </style>
